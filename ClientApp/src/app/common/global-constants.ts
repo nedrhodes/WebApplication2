654a13e9b9142f133fb550c73c3b11cca61c19b4
Ned Rhodes <ned@softwaresystemsgroup.com>
2023-02-10 15:50:22 -0500

    Change label to Angular 15

--- ClientApp/src/app/common/global-constants.ts
+++ ClientApp/src/app/common/global-constants.ts
@@ -1,5 +1,5 @@
 export class GlobalConstants {
-  static appTitle = "Angular 13 Application";
+  static appTitle = "Angular 15 Application";
   static author = "Ned W. Rhodes";
 
   static baseWebAPI = 'http://10.26.144.41:5002';
